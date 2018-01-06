9821471c2e0d9fb7cb5c4392e1a39e1283e6c12b
hdwilber <hd.wilber@gmail.com>
2018-01-06 01:05:59 -0400

    The initial working setup has been set

---
+++ src/containers/edumiga.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Header } from 'semantic-ui-react'
+
+class Edumiga extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <Header size="large">Welcome</Header>
+  }
+}
+
+export default Edumiga
---
+++ src/containers/index.js
@@ -0,0 +1,1 @@
+export { default as Edumiga } from './edumiga'
